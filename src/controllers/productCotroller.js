fe90bf4f9802e9ca831ea24338a9a2bae0a8f5d7
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-09-11 23:06:15 -0300

    create product finalizdo

--- src/controllers/productCotroller.js
+++ src/controllers/productCotroller.js
@@ -1,3 +1,28 @@
  import { Prisma } from "../lib/prisma";
 
- 
+ export const createProduct = async (req,res) =>{
+    try {
+        const {nome,sku,quantity,minQuantity,description,price,categoryId} = req.body
+
+        if (!nome || !sku || !price || !quantity||!categoryId){
+            return res.status(400).json({ message :'Campos Obrigatorios faltando '})
+        }
+
+        const newProduct = await Prisma.product.create({
+            data :{
+                nome,
+                sku,
+                description,
+                price,
+                quantity,
+                minQuantity,
+                categoryId
+            }
+        })
+        res.status(201).json(newProduct)
+    }
+    catch (error){
+        res.status(500).json({message :'Erro Ao Criar Novo Produto', error : error.message})
+    }
+    
+ }
