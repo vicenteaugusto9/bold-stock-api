f371e9032de462b2ca18876267839b92bbd7670a
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-09-13 15:15:55 -0300

    CRUD incial finalizado

--- src/controllers/productCotroller.js
+++ src/controllers/productCotroller.js
@@ -26,3 +26,55 @@
     }
     
  }
+
+ export const listarProductsPorID = async (req,res) => {
+    try{
+        const {id} = req.params
+        const product =  await Prisma.product.findUnique({
+            where:{id},
+            include : {
+                categoryId: true
+            }
+        })
+        if (!product){
+            return res.status(404).json({message : 'Produto Nao Encontrado  '})
+        }
+        res.status(200).json(product)
+    } catch (error){
+        res.status(500).json({message: 'error ao buscar produto', error : error.message})
+    }
+ } 
+
+ export const atualizarProduct = async  (req,res) => {
+    try {
+        const {id} = req.params
+        const dataoUpdate = req.body
+
+        const atualizarProduct =  await Prisma.product.update({
+                where : {id},
+                data: dataoUpdate
+        })
+        res.status(200).json(atualizarProduct)
+    } catch (error){
+        if (error.code === 'P2025'){
+            return res.status(404).json({message :' Produto Nao Encotrado Para Atualizacao'})
+        }
+        res.status(500).json({message: 'Produto Nao Encontrado', error : error.message})
+    }
+ }
+
+ export const deletarProduct = async (req,res) =>{
+    try{
+        const {id} = req.params
+
+        await Prisma.product.delete({
+            where: {id}
+        })
+        res.status(204).send()
+    } catch(error){
+        if(error.code === 'P2025'){
+            return res.status(404).json({message : 'Produto Nao encontrado para delete'})
+        }
+        res.status(500).json({message: 'Nao foi possivel deletar produto', error :error.message})
+    }
+ }
